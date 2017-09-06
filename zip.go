7fc64a598db7cf7257063fd76ff302029ac0a3e5
Karn Sakulsak <karn@thinknet.co.th>
2017-09-06 16:02:45 +0700

    Code format adjust: Change tab to space

--- zip.go
+++ zip.go
@@ -1,75 +1,70 @@
+// Inspired by code from
+// * https://gist.github.com/superbrothers/0a8b6390c6315916aeb8
+// * https://golang.org/pkg/archive/zip/#example_Writer
 package main
 
-/*
-
-Inspired by code from
-* https://gist.github.com/superbrothers/0a8b6390c6315916aeb8
-* https://golang.org/pkg/archive/zip/#example_Writer
-
-*/
-
 import (
-	"fmt"
-	"archive/zip"
-	"log"
-	"bytes"
-	"net/http"
+    "fmt"
+    "archive/zip"
+    "log"
+    "bytes"
+    "net/http"
 )
 
 func main () {
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe("localhost:4001", nil)
+    http.HandleFunc("/", rootHandler)
+    http.ListenAndServe("localhost:4001", nil)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	var err error
+    var err error
 
-	w.Header().Set("Content-Type", "application/zip")
-	w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/zip")
+    w.WriteHeader(http.StatusOK)
 
-	// Create a buffer to write our archive to.
-	buf := new(bytes.Buffer)
-	
-	// Create a new zip archive.
-	zipWriter := zip.NewWriter(buf)
+    // Create a buffer to write our archive to.
+    buf := new(bytes.Buffer)
 
-	// Add some files to the archive.
-	var files = []struct {
-			Name, Body string
-	}{
-			{"test/readme.txt", "This archive contains some text files."},
-			{"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
-			{"todo.txt", "Get animal handling licence.\nWrite more examples."},
-	}
-	for _, file := range files {
-		f, err := zipWriter.Create(file.Name)
-		if err != nil {
-				log.Fatal(err)
-		}
+    // Create a new zip archive.
+    zipWriter := zip.NewWriter(buf)
 
-		var buffer []byte
+    // Add some files to the archive.
+    var files = []struct {
+            Name, Body string
+    }{
+            {"test/readme.txt", "This archive contains some text files."},
+            {"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
+            {"todo.txt", "Get animal handling licence.\nWrite more examples."},
+    }
+    for _, file := range files {
+        f, err := zipWriter.Create(file.Name)
+        if err != nil {
+                log.Fatal(err)
+        }
 
-		buffer = []byte(file.Body)
+        var buffer []byte
 
-		// Content of buffer to be written can be read from file by ioutil.ReadFile
-		// content, err := ioutil.ReadFile(file.Name)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// } else {
-		// 	buffer = content
-		// }
+        buffer = []byte(file.Body)
 
-		_, err = f.Write(buffer)
-		if err != nil {
-				log.Fatal(err)
-		}
-	}
+        // Content of buffer to be written can be read from file by ioutil.ReadFile
+        // content, err := ioutil.ReadFile(file.Name)
+        // if err != nil {
+        //     log.Fatal(err)
+        // } else {
+        //     buffer = content
+        // }
 
-	// Make sure to check the error on Close.
-	err = zipWriter.Close()
-	if err != nil {
-			log.Fatal(err)
-	}
+        _, err = f.Write(buffer)
+        if err != nil {
+                log.Fatal(err)
+        }
+    }
 
-	fmt.Fprintf(w, "%s", buf.String())
+    // Flush all added content to buffer and close writer
+    err = zipWriter.Close()
+    if err != nil {
+            log.Fatal(err)
+    }
+
+    fmt.Fprintf(w, "%s", buf.String())
 }
